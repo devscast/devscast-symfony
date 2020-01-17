6d6c5a30bcc0bb639daade898babbc7df2f430d8
bernard-ng <ngandubernard@gmail.com>
2020-01-18 01:18:07 +0200

    feat: run fixtures only in dev environment

--- src/DataFixtures/BlogFixtures.php
+++ src/DataFixtures/BlogFixtures.php
@@ -18,29 +18,31 @@ class BlogFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = $manager
-            ->getRepository(User::class)
-            ->findOneBy(['email' => 'admin@devs-cast.com']);
+        if ($_ENV['APP_ENV'] === 'dev') {
+            $user = $manager
+                ->getRepository(User::class)
+                ->findOneBy(['email' => 'admin@devs-cast.com']);
 
-        if ($user) {
-            $faker = Factory::create('fr_FR');
-            for ($i = 0; $i < 100; $i++) {
-                $blog = new Blog();
-                $blog
-                    ->setName($faker->words(4, true))
-                    ->setContent($faker->sentences(3, true))
-                    ->setCreatedAt(new \DateTime("now"))
-                    ->setIsOnline(true)
-                    ->setThumbUrl("default.jpg")
-                    ->setIsArchived(0)
-                    ->setSlug($faker->slug)
-                    ->setUser($user);
-                $manager->persist($blog);
+            if ($user) {
+                $faker = Factory::create('fr_FR');
+                for ($i = 0; $i < 100; $i++) {
+                    $blog = new Blog();
+                    $blog
+                        ->setName($faker->words(4, true))
+                        ->setContent($faker->sentences(3, true))
+                        ->setCreatedAt(new \DateTime("now"))
+                        ->setIsOnline(true)
+                        ->setThumbUrl("default.jpg")
+                        ->setIsArchived(0)
+                        ->setSlug($faker->slug)
+                        ->setUser($user);
+                    $manager->persist($blog);
+                }
+
+                $manager->flush();
+            } else {
+                throw new \LogicException("default User doesn't exist, please create one by running UserFixtures");
             }
-
-            $manager->flush();
-        } else {
-            throw new \LogicException("default User doesn't exist, please create one by running UserFixtures");
         }
     }
 }
