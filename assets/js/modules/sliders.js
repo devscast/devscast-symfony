eb1cfd017ae04662fa55335fbf20858e81f33032
bernard-ng <ngandubernard@gmail.com>
2020-01-24 02:43:40 +0200

    fix: remove unused sliders

--- assets/js/modules/sliders.js
+++ assets/js/modules/sliders.js
@@ -20,26 +20,6 @@ const defaultConfig = {
     ],
 };
 
-$('.projects-slides').owlCarousel({
-    ...defaultConfig,
-    mouseDrag: true,
-    margin: 30,
-});
-
-$('.testimonials-slides').owlCarousel({
-    ...defaultConfig,
-    items: 1,
-    animateOut: 'fadeOut',
-    responsive: {},
-});
-
-$('.team-slides').owlCarousel({
-    ...defaultConfig,
-    autoplayHoverPause: true,
-    mouseDrag: true,
-    margin: 30,
-});
-
 $('.projects-image-slides').owlCarousel({
     ...defaultConfig,
     autoplayHoverPause: true,
@@ -49,18 +29,5 @@ $('.projects-image-slides').owlCarousel({
         0: {items: 1},
         768: {items: 2},
         1200: {items: 2},
-    }
-});
-
-$('.partner-slides').owlCarousel({
-    ...defaultConfig,
-    autoplayHoverPause: true,
-    mouseDrag: true,
-    margin: 30,
-    responsive: {
-        0: {items: 2},
-        576: {items: 3},
-        768: {items: 4},
-        1200: {items: 6}
     }
 });
