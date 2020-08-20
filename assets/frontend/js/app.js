a483b8628659fabb2d02bc7b3699998ebf3ea6d2
bernard-ng <ngandubernard@gmail.com>
2020-08-20 21:50:54 +0200

    feat(design): add tailwind config

--- assets/frontend/js/app.js
+++ assets/frontend/js/app.js
@@ -1,12 +1,1 @@
-// Style
-import "../sass/app.scss";
-
-// Bootstrap modules
-import 'bootstrap/js/dist/collapse'
-import 'bootstrap/js/dist/alert'
-import 'bootstrap/js/dist/modal'
-
-// App modules
-import './modules/application'
-import './modules/progress'
-import './modules/sliders'
+import '../sass/app.scss';
