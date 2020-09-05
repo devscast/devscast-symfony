65c6c7159184093a627d05ea5e18dfeb48d7ce66
scotttresor <scotttresor@gmail.com>
2020-09-05 19:54:50 +0200

    feat(conduct):implemente  conduct code

--- assets/frontend/js/app.js
+++ assets/frontend/js/app.js
@@ -1,1 +1,6 @@
 import '../sass/app.scss';
+
+import '@splidejs/splide/dist/css/splide.min.css';
+import '@splidejs/splide/dist/css/splide-core.min.css';
+
+import '@splidejs/splide/dist/js/splide';
